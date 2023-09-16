d35a5e20936aa73a3d11e6a99ead5bab7d4d300f
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-16 14:34:40 +0530

    jenkins initial commit

---
+++ WebApiJenkins/Data/WebApiJenkinsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiJenkins.Models;
+
+namespace WebApiJenkins.Data
+{
+    public class WebApiJenkinsContext : DbContext
+    {
+        public WebApiJenkinsContext (DbContextOptions<WebApiJenkinsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiJenkins.Models.Movie> Movie { get; set; } = default!;
+    }
+}
